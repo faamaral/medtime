c4b971bccf395c5a2eb7aa34cc52a8feba700258
Fabiano Amaral Alves <fabianoamaral445@gmail.com>
2024-11-11 22:18:29 -0300

    Initial setup

---
+++ app/src/main/java/com/digitaldose/medtime/viewmodels/MedicamentoViewModel.kt
@@ -0,0 +1,12 @@
+package com.digitaldose.medtime.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Fabiano Amaral Alves <fabianoamaral445@gmail.com>
+ * @since 11/11/2024
+ */
+
+class MedicamentoViewModel : ViewModel() {
+
+}
